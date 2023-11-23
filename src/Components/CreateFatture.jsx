6ae69088746c38063e8ba3ba25537239d3b8829e
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-11-23 15:35:39 +0100

    inizio createFattura

--- src/Components/CreateFatture.jsx
+++ src/Components/CreateFatture.jsx
@@ -6,15 +6,20 @@ const CreateFatture = () => {
   const dispatch = useDispatch();
   const [validated, setValidated] = useState(false);
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
   const handleShow = () => {
     setShow(true);
   };
+  const [formData, setFormData] = useState({
+    clienteID: "",
+    importo: "",
+    numeroFattura: "",
+  });
+
   return (
     <>
       <Button onClick={handleShow}>Aggiungi</Button>
       <Modal show={show} onHide={handleClose} className="mt-3">
-        <Form noValidate validated={validated}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Modal.Header>
             <Modal.Title>
               <p>Crea Una nuova fattura</p>
@@ -22,22 +27,39 @@ const CreateFatture = () => {
           </Modal.Header>
           <Modal.Body>
             <Form.Group className="mb-3" controlId="validationCustom01">
-              <Form.Label>cliente ID</Form.Label>
-              <Form.Control type="text" />
+              <Form.Label>Cliente ID</Form.Label>
+              <Form.Control type="text" name="clienteID" value={formData.clienteID} onChange={handleChange} required />
+              <Form.Control.Feedback type="invalid">Campo obbligatorio</Form.Control.Feedback>
             </Form.Group>
-
             <Form.Group className="mb-3" controlId="validationCustom02">
               <Form.Label>Importo</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" name="importo" value={formData.importo} onChange={handleChange} required />
+              <Form.Control.Feedback type="invalid">Campo obbligatorio</Form.Control.Feedback>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="validationCustom02">
-              <Form.Label>Numero fattura</Form.Label>
-              <Form.Control type="text" />
+            <Form.Group className="mb-3" controlId="validationCustom03">
+              <Form.Label>Numero Fattura</Form.Label>
+              <Form.Control
+                type="text"
+                name="numeroFattura"
+                value={formData.numeroFattura}
+                onChange={handleChange}
+                required
+              />
+              <Form.Control.Feedback type="invalid">Campo obbligatorio</Form.Control.Feedback>
             </Form.Group>
           </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Chiudi
+            </Button>
+            <Button type="submit" variant="primary">
+              Salva
+            </Button>
+          </Modal.Footer>
         </Form>
       </Modal>
     </>
   );
 };
+
 export default CreateFatture;
